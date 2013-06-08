0ae1d9c1c1bc5cec8b8a058be2f0fdacc847b6ce
Eladio Ruiz <eladioruiz@gmail.com>
2013-06-08 21:01:05 +0200

    Documentation: requisitions, images, etc

---
+++ Documents/Gym4All.mm
@@ -0,0 +1,76 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1369599128039" ID="ID_1805374822" MODIFIED="1369772867253" STYLE="fork" TEXT="Gym4All">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1369599386657" ID="ID_1620820387" MODIFIED="1369772868231" POSITION="right" STYLE="fork" TEXT="Gym">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<attribute NAME="Web" VALUE=""/>
+<node COLOR="#00b439" CREATED="1369599526216" ID="ID_1944367936" MODIFIED="1369772868234" TEXT="Plantillas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1369599543670" ID="ID_1281366334" MODIFIED="1369764549418" TEXT="Grupos Musculares">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1369599560216" ID="ID_1181739808" MODIFIED="1369764549419" TEXT="Tablas">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1369599551248" ID="ID_177709855" MODIFIED="1369764549419" STYLE="fork" TEXT="Ejercicios">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1369599793297" ID="ID_1479236336" MODIFIED="1369764549419" TEXT="Monitores">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1369599804744" ID="ID_1801798631" MODIFIED="1369764549420" TEXT="Peticiones">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1369599421456" ID="ID_602094713" MODIFIED="1369772868235" POSITION="left" TEXT="Client">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<attribute NAME="Android" VALUE=""/>
+<attribute NAME="iOs" VALUE=""/>
+<node COLOR="#00b439" CREATED="1369599728273" ID="ID_894309962" MODIFIED="1369772868238" TEXT="Tabla">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1369600113076" ID="ID_719624676" MODIFIED="1369764549421" TEXT="Periodo">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1369600127976" ID="ID_1276474537" MODIFIED="1369772872291" TEXT="Grupos">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1369600222434" ID="ID_827850927" MODIFIED="1369764549421" TEXT="Cardio">
+<attribute NAME="Tiempo" VALUE=""/>
+</node>
+<node COLOR="#111111" CREATED="1369600288247" ID="ID_922670125" MODIFIED="1369764549421" TEXT="Abdominales">
+<attribute NAME="Repeticiones" VALUE=""/>
+</node>
+<node COLOR="#111111" CREATED="1369600346397" ID="ID_77943093" MODIFIED="1369772872516" TEXT="Grupos Musculares">
+<attribute_layout NAME_WIDTH="133"/>
+<attribute NAME="Peso" VALUE=""/>
+<attribute NAME="Repeticiones iniciales" VALUE=""/>
+<attribute NAME="Repeticiones realizadas" VALUE=""/>
+<node COLOR="#111111" CREATED="1369600371473" ID="ID_1670692743" MODIFIED="1369764549421" TEXT="Pecho"/>
+<node COLOR="#111111" CREATED="1369600377281" ID="ID_1347402058" MODIFIED="1369764549422" TEXT="Espalda"/>
+<node COLOR="#111111" CREATED="1369600383248" ID="ID_169381001" MODIFIED="1369764549422" TEXT="Biceps"/>
+<node COLOR="#111111" CREATED="1369600390879" ID="ID_1055646175" MODIFIED="1369764549422" TEXT="Triceps"/>
+<node COLOR="#111111" CREATED="1369600402026" ID="ID_1716850049" MODIFIED="1369764549422" TEXT="Hombros"/>
+<node COLOR="#111111" CREATED="1369600408074" ID="ID_1492105126" MODIFIED="1369764549422" TEXT="Piernas"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1369599845709" ID="ID_124325198" MODIFIED="1369764549422" TEXT="Peticiones">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1369600095120" ID="ID_379239572" MODIFIED="1369764549423" TEXT="Estad&#xed;sticas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
